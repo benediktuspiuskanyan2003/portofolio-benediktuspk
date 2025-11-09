b990b2703ca89ace88c016867505c0502c72df54
benediktuspiuskanyan2003 <benediktuspiuskanyan2003@gmail.com>
2025-11-09 16:58:01 +0700

    ganti dengan Skills

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import About from "./components/About"; 
-import MySkills from "MySkills";
+import MySkills from "./components/Skills.jsx";
 import Footer from "./components/Footer";
 
 function App() {
